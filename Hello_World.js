4e4dfd41eee989434e2033ba7b0587e02966504a
mdsalman19 <salmanqep@gmail.com>
2025-02-02 16:14:47 +0530

    Create Hello_World.js

---
+++ Hello_World.js
@@ -0,0 +1,28 @@
+// Print function
+console.log("Hello World!");
+console.log("This is my beginning of learning in JavaScript");
+console.log("I'll have to complete JavaScript before March 1st.");
+
+// Variables
+// Three types of words can be used to assign variables:
+// Let, var, and const
+// var is universal, let and const are local variables
+var a = 56;
+var a = 52; // var allows redeclaration  
+console.log(a);
+
+let b = 45;
+console.log(b); // This should print 45
+
+const w = "Very Good Job"
+console.log(w)
+// const w = "Do it"  block scope variable and also non redeclarable
+// 7 types of datat types:
+String1 = "String Data type"
+Boolean1  = true
+Number1 = 456
+H = null
+o = undefined
+Symbol1 = "Symbol"
+BigInt1 = BigInt(456654)
+console.log(String1, Boolean1, H, Number1, Symbol1, BigInt1, o)
